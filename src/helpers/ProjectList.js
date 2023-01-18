29c38305c2b7c06765212bc7c1680fcddd76ef97
Murali07 <muraliselvaraj97@gmail.com>
2023-01-18 16:12:50 +0530

    project added

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,6 +1,9 @@
 import Movie from "../assets/movie.jpeg";
-import Ecommerce from "../assets/ecommerce.jpg";
+// import Ecommerce from "../assets/ecommerce.jpg";
 import Mobile from "../assets/mobile.jpg";
+import Helpdesk from "../assets/helpdesk.jpg";
+// import SupportDesk from "../assets/supportDesk.jpg";
+import FoodApp from "../assets/foodapp4.jpg";
 
 export const ProjectList = [
   {
@@ -10,13 +13,13 @@ export const ProjectList = [
     demo: "https://lovely-tartufo-59ae96.netlify.app/",
     link: "https://github.com/Murali07/day21_assignment",
   },
-  {
-    name: "Ecommerce Website",
-    image: Ecommerce,
-    skills: "ReactJS, NodeJS, HTML, CSS",
-    demo: "https://brilliant-truffle-7b0e9c.netlify.app",
-    link: "https://github.com/Murali07/webscrapping-ecommerce",
-  },
+  // {
+  //   name: "Ecommerce Website",
+  //   image: Ecommerce,
+  //   skills: "ReactJS, NodeJS, HTML, CSS",
+  //   demo: "https://brilliant-truffle-7b0e9c.netlify.app",
+  //   link: "https://github.com/Murali07/webscrapping-ecommerce",
+  // },
   {
     name: "Mobile Shop",
     image: Mobile,
@@ -24,4 +27,18 @@ export const ProjectList = [
     demo: "https://shiny-frangipane-7b12ed.netlify.app",
     link: "https://github.com/Murali07/fsd_demo",
   },
+  {
+    name: "Zendesk App",
+    image: Helpdesk,
+    skills: "React.js, Node.js, Express.js, MongoDB, Mongoose, ReactBootstrap",
+    demo: "https://zendesk-application.netlify.app",
+    link: "https://github.com/Murali07/ZenDesk.git"
+  },
+  {
+    name: "Food Delivery App",
+    image: FoodApp,
+    skills: "React.js, Node.js, Express.js, MongoDB, Mongoose, ReactBootstrap, JWT",
+    demo: "https://sssm-food-app.netlify.app",
+    link: "https://github.com/Murali07/Food-App.git"
+  }
 ];
